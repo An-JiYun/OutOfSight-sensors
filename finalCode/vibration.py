df27517a5a325590834a91ec04e14a3e185048d5
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-02-20 01:19:55 -0500

    Update vibration.py

--- finalCode/vibration.py
+++ finalCode/vibration.py
@@ -19,9 +19,9 @@ try:
         CUR_TIME = time.time()
 
         # 1초마다 진동 카운트 검사 및 리스트에 추가
-        if CUR_TIME - PREV_TIME > 1:
+        if CUR_TIME - PREV_TIME > 2:
             print("Vibration count in last 1 seconds:", i)
-            if i > 0: #내지는 1
+            if i > 1
                 with open(filename, "w") as file:
                     file.write("true")
                 print(f"vibration Data written to file (3 value in list)")
