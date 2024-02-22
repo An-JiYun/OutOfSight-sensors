271d1bc78d8491d6287040aed4072ffc1128da67
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-02-22 10:35:44 -0500

    Update firebase.py

--- finalCode/firebase.py
+++ finalCode/firebase.py
@@ -83,3 +83,117 @@ def sensor_monitor():
 
 sensor_thread = threading.Thread(target=sensor_monitor)
 sensor_thread.start()
+
+
+# import time
+# import serial
+# import os
+# import json
+# from firebase_admin import credentials, firestore, initialize_app
+# from datetime import datetime
+# import threading
+# import paho.mqtt.client as mqtt
+
+# # ThingsBoard 설정
+# THINGSBOARD_HOST = '127.0.0.1'
+# ACCESS_TOKEN = 'RyENpPtJU2gVF7PLHSQn'
+
+# # MQTT 클라이언트 설정
+# client = mqtt.Client()
+# client.username_pw_set(ACCESS_TOKEN)
+# client.connect(THINGSBOARD_HOST, 1883, 60)
+# client.loop_start()
+
+# # Firebase 초기화
+# cred = credentials.Certificate('/home/rpi/firebase_keys/out-of-sight-814f2-firebase-adminsdk-3ozx8-8e66ae3f96.json')
+# initialize_app(cred)
+# db = firestore.client()
+
+# # 센서 파일 경로
+# vibration_file = "/home/rpi/Documents/sensors/text/vibration_data.txt"
+# pressure_file = "/home/rpi/Documents/sensors/text/pressure_data.txt"
+# gps_file = "/home/rpi/Documents/sensors/text/gps_data.txt"
+
+# # 이전 GPS 데이터 저장 변수 초기화
+# prev_gps_data = (0, 0)
+
+# def read_and_send_data():
+#     vibration, vibration_value = read_sensor_data(vibration_file)
+#     pressure, pressure_value = read_sensor_data(pressure_file)
+#     gps_data, gps_signal = read_gps_data()
+
+#      # ThingsBoard에 데이터 전송
+#     send_data_to_thingsboard(vibration_value, pressure_value, gps_data, gps_signal)
+
+#     # 값이 변한, 세 값 중 하나만 True인 경우에만 Firebase에 데이터 전송
+#     if vibration or pressure or gps_signal:
+#         send_data_to_firebase(vibration, pressure, gps_data, gps_signal)
+
+
+
+# def read_sensor_data(file_path):
+#     if os.path.exists(file_path):
+#         with open(file_path, "r") as file:
+#             data = file.read().strip().split()
+#         os.remove(file_path)
+#         if "true" in data[0]:
+#             return True, data[1:]
+#         else:
+#             return False, []
+#     return False, []
+
+# def read_gps_data():
+#     #GPS 파일에서 데이터 읽기 또는 이전 값 사용
+#     global prev_gps_data
+#     if os.path.exists(gps_file):
+#         with open(gps_file, "r") as file:
+#             lat, lng = file.read().strip().split()
+#         prev_gps_data = (float(lat), float(lng))
+#         os.remove(gps_file)
+#         return prev_gps_data, True
+#     return prev_gps_data, False
+
+
+# def send_data_to_firebase(vibration, pressure, gps_data, gps_signal):
+#     #파이어베이스에 데이터 전송
+#     # gps_signal이 False일 때도 prev_gps_data 사용
+#     gps_lat, gps_lng = gps_data if gps_signal else prev_gps_data
+#     # Firestore의 GeoPoint 생성을 위해 gps_lat, gps_lng 사용
+#     geopoint = firestore.GeoPoint(gps_lat, gps_lng)
+
+#     data = {
+#         'isChecked': False,
+#         'geopoint': geopoint,
+#         'signal': {
+#             'gps': gps_signal,
+#             'pressure': pressure,
+#             'vibration': vibration,
+#             'stranger': False
+#         },
+#         'time': firestore.SERVER_TIMESTAMP,
+#         'video' : ""
+#     }
+
+#     doc_ref = db.collection('Vehicles').document('1234-ID').collection('Notifications').document()
+#     doc_ref.set(data)
+#     print("Sensor data sent to Firebase.")
+
+
+# def send_data_to_thingsboard(vibration_value, pressure_value, gps_data):
+#     telemetry_data = {
+#         "vibration": float(vibration_value[0]) if vibration_value else 0,
+#         "pressure": [float(p) for p in pressure_value] if pressure_value else [0, 0, 0, 0],
+#         "gps_latitude": gps_data[0],
+#         "gps_longitude": gps_data[1]
+#     }
+#     client.publish('v1/devices/me/telemetry', json.dumps(telemetry_data))
+#     print("Sensor data sent to ThingsBoard.")
+
+
+# def sensor_monitor():
+#     while True:
+#         read_and_send_data()
+#         time.sleep(1)
+
+# sensor_thread = threading.Thread(target=sensor_monitor)
+# sensor_thread.start()
