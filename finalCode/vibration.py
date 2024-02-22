9684f0fbf8dd9d2be2a79d4a77cbbcb8989be953
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-02-22 10:34:50 -0500

    Update vibration.py

--- finalCode/vibration.py
+++ finalCode/vibration.py
@@ -18,16 +18,34 @@ try:
 
         CUR_TIME = time.time()
 
-        # 1초마다 진동 카운트 검사 및 리스트에 추가
+        # 2초마다 진동 카운트 검사 (안함)
         if CUR_TIME - PREV_TIME > 2:
-            print("Vibration count in last 1 seconds:", i)
-            if i > 1
-                with open(filename, "w") as file:
-                    file.write("true")
-                print(f"vibration Data written to file (3 value in list)")
+            print("Vibration count in last 2 seconds:", i)
             
+            if i > 1:
+                with open(filename, "w") as file:
+                    # file.write(f"true {i}")
+                    file.write(f"true")
+                print(f"vibration Data written to file")
+                
             i = 0
+            
             PREV_TIME = CUR_TIME
+            
+            # if i > 20:
+            #     vibration_counts.append(i)
+            # i = 0
+            # PREV_TIME = CUR_TIME
+
+            # # 리스트에 3개 값이 있으면 파이어베이스에 데이터 보내기
+            # if len(vibration_counts) >= 3:
+            #     with open(filename, "w") as file:
+            #         file.write("true")
+            #     print(f"vibration Data written to file (3 value in list)")
+                
+            #     # 10초 동안 멈추기
+            #     time.sleep(10)
+            #     vibration_counts = []  # 리스트 초기화
 
         time.sleep(0.01)
 
